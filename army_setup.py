1555f4183a3fb9249e7c92c378c358e0bd1d7714
Andrew Salter <ajsal3@student.monash.edu>
2015-10-08 20:44:58 +1100

    created validation for army_setup

--- army_setup.py
+++ army_setup.py
@@ -2,10 +2,52 @@ from Army import Army
 
 def player_setup():
     name = input("\tEnter your name: ")
+    gold = 20
 
-    n_soldiers = int(input("How many soldiers would you like?: "))
-    n_archers = int(input("How many archers would you like?: "))
-    n_cavalry = int(input("How many cavalrymen would you like?: "))
+    while True:
+        try:
+            n_soldiers = int(input("\tHow many soldiers would you like?: "))
+            if n_soldiers < 0:
+                raise ValueError
+        except ValueError:
+            print("\t!! Invalid input. Must be a natural number.")
+            continue
+
+        if gold - n_soldiers < 0:
+            print("\t!! Not enough funds.")
+            continue
+
+        gold -= n_soldiers
+
+    while True:
+        try:
+            n_archers = int(input("\tHow many archers would you like?: "))
+            if n_archers < 0:
+                raise ValueError
+        except ValueError:
+            print("\t!! Invalid input. Must be a natural number.")
+            continue
+
+        if gold - (n_archers * 2) < 0:
+            print("\t!! Not enough funds.")
+            continue
+
+        gold -= n_archers * 2
+
+    while True:
+        try:
+            n_cavalry = int(input("\tHow many cavalrymen would you like?: "))
+            if n_cavalry < 0:
+                raise ValueError
+        except ValueError:
+            print("\t!! Invalid input. Must be a natural number.")
+            continue
+
+        if gold - (n_cavalry * 3) < 0:
+            print("\t!! Not enough funds.")
+            continue
+
+        gold -= n_cavalry * 3
 
     return Army(name, n_soldiers, n_archers, n_cavalry)
 
